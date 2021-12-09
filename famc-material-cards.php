ff97a2a2fb6a7b4d1d22ed1b0c2f1a6ef5bb25ab
Farhan Ahmed <56694327+iamrealfarhanbd@users.noreply.github.com>
2021-12-09 16:51:17 +0600

    Update famc-material-cards.php

--- famc-material-cards.php
+++ famc-material-cards.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name:       FA MC
- * Plugin URI:        https://github.com/wpacademy/fa-mc
+ * Plugin URI:        https://github.com/iamrealfarhanbd/famc-material-cards.php
  * Description:       Beautful Material Cards widget for Elementor Prectice.
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Farhan Ahmed
- * Author URI:        https://twitter.com/MSRwpac
+ * Author URI:        https://bd.linkedin.com/in/iamrealfarhanbd
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       fa-mc
